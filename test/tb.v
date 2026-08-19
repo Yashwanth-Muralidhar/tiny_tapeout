a844805cb8b7acbf86b475099a5f92ddab58a8e8
Yashwanth-Muralidhar <yashwanthmuralidhar@gmail.com>
2026-08-19 15:16:55 +0530

    Update tb.v

--- test/tb.v
+++ test/tb.v
@@ -4,7 +4,7 @@ module tb;
     reg [7:0] ui_in, uio_in;
     wire [7:0] uo_out, uio_out, uio_oe;
 
-    tt_um_vinayaka_pqc_fo_v8 user_project (
+   tt_um_vinayaka_pqc_fo user_project (
         .ui_in(ui_in), .uo_out(uo_out), .uio_in(uio_in),
         .uio_out(uio_out), .uio_oe(uio_oe),
         .ena(ena), .clk(clk), .rst_n(rst_n)
