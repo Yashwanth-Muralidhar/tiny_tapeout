d92a92074789fadc6b79f2ab6c53eefad1de5e93
Yashwanth-Muralidhar <yashwanthmuralidhar@gmail.com>
2026-08-19 09:42:18 +0530

    Update tb.v

--- test/tb.v
+++ test/tb.v
@@ -1,43 +1,46 @@
-// SPDX-FileCopyrightText: (c) 2026 H Vinayaka
-// SPDX-License-Identifier: Apache-2.0
+`timescale 1ns/1ps
 `default_nettype none
-`timescale 1ns / 1ps
 
-module tb ();
+`include "../src/project_opt1_isolated_renamed.v"
 
-  initial begin
-    $dumpfile("tb.vcd");
-    $dumpvars(0, tb);
-    #1;
-  end
+module tb;
 
-  reg clk;
-  reg rst_n;
-  reg ena;
-  reg  [7:0] ui_in;
-  reg  [7:0] uio_in;
-  wire [7:0] uo_out;
-  wire [7:0] uio_out;
-  wire [7:0] uio_oe;
+    reg  [7:0] ui_in;
+    wire [7:0] ref_uo_out;
+    wire [7:0] ref_uio_out;
+    wire [7:0] ref_uio_oe;
 
-`ifdef GL_TEST
-  wire VPWR = 1'b1;
-  wire VGND = 1'b0;
-`endif
+    wire [7:0] opt_uo_out;
+    wire [7:0] opt_uio_out;
+    wire [7:0] opt_uio_oe;
 
-  tt_um_vinayaka_pqc_fo user_project (
-`ifdef GL_TEST
-      .VPWR(VPWR),
-      .VGND(VGND),
-`endif
-      .ui_in  (ui_in),
-      .uo_out (uo_out),
-      .uio_in (uio_in),
-      .uio_out(uio_out),
-      .uio_oe (uio_oe),
-      .ena    (ena),
-      .clk    (clk),
-      .rst_n  (rst_n)
-  );
+    reg  [7:0] uio_in;
+    reg        ena;
+    reg        clk;
+    reg        rst_n;
+
+    tt_um_vinayaka_pqc_fo ref (
+        .ui_in   (ui_in),
+        .uo_out  (ref_uo_out),
+        .uio_in  (uio_in),
+        .uio_out (ref_uio_out),
+        .uio_oe  (ref_uio_oe),
+        .ena      (ena),
+        .clk      (clk),
+        .rst_n    (rst_n)
+    );
+
+    tt_um_vinayaka_pqc_fo_opt opt (
+        .ui_in   (ui_in),
+        .uo_out  (opt_uo_out),
+        .uio_in  (uio_in),
+        .uio_out (opt_uio_out),
+        .uio_oe  (opt_uio_oe),
+        .ena      (ena),
+        .clk      (clk),
+        .rst_n    (rst_n)
+    );
 
 endmodule
+
+`default_nettype wire
