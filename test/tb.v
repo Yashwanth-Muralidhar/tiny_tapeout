cc5a5cecf47f9fb40a561120ace2a0a39810cc9f
Yashwanth-Muralidhar <yashwanthmuralidhar@gmail.com>
2026-08-19 14:39:37 +0530

    Update tb.v

--- test/tb.v
+++ test/tb.v
@@ -1,33 +1,12 @@
-`default_nettype none
 `timescale 1ns / 1ps
+module tb;
+    reg clk, rst_n, ena;
+    reg [7:0] ui_in, uio_in;
+    wire [7:0] uo_out, uio_out, uio_oe;
 
-// cocotb testbench wrapper for the ACTUAL TT top module.
-// Fixes the old ml_kem_decap / ref_uo_out mismatch.
-module tb ();
-
-  initial begin
-    $dumpfile("tb.vcd");
-    $dumpvars(0, tb);
-  end
-
-  reg        clk;
-  reg        rst_n;
-  reg        ena;
-  reg  [7:0] ui_in;
-  reg  [7:0] uio_in;
-  wire [7:0] uo_out;
-  wire [7:0] uio_out;
-  wire [7:0] uio_oe;
-
-  tt_um_vinayaka_pqc_fo user_project (
-      .ui_in  (ui_in),
-      .uo_out (uo_out),
-      .uio_in (uio_in),
-      .uio_out(uio_out),
-      .uio_oe (uio_oe),
-      .ena    (ena),
-      .clk    (clk),
-      .rst_n  (rst_n)
-  );
-
+    tt_um_vinayaka_pqc_fo_v8 user_project (
+        .ui_in(ui_in), .uo_out(uo_out), .uio_in(uio_in),
+        .uio_out(uio_out), .uio_oe(uio_oe),
+        .ena(ena), .clk(clk), .rst_n(rst_n)
+    );
 endmodule
