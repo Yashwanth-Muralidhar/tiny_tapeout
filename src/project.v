4ec1c3d496fddd9bc4a7bc52f4f30817c77e2dd4
Yashwanth-Muralidhar <yashwanthmuralidhar@gmail.com>
2026-08-19 11:34:52 +0530

    Update project.v

--- src/project.v
+++ src/project.v
@@ -7,7 +7,7 @@
 
 module tt_um_vinayaka_pqc_fo (
     input  wire [7:0] ui_in,
-    output wire [7:0] uo_out,
+    output wire [7:0] uo_out, 
     input  wire [7:0] uio_in,
     output wire [7:0] uio_out,
     output wire [7:0] uio_oe,
